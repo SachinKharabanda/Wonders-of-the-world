2d93f63c4ce8a2abe423efa98b21254be2e7316a
Sachin <sachin.kharabanda@aol.com>
2025-02-05 12:46:49 +0000

    Vite config and package.json changes

--- wonders/vite.config.js
+++ wonders/vite.config.js
@@ -3,7 +3,8 @@ import plugin from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+  base: '/Wonders-of-the-world/',
+  plugins: [plugin()],
     server: {
         port: 50232,
     }
